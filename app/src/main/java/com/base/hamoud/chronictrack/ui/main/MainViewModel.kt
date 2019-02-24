ff36fe4e9df80a4f011e261588236864041b6d0f
Moe <m.hamoudd@gmail.com>
2019-02-24 18:12:12 -0500

    task/mvvm clean up MainViewModel by creating abstract BaseAndroidViewModel and movind coroutine ioScope and uiScope there

--- app/src/main/java/com/base/hamoud/chronictrack/ui/main/MainViewModel.kt
+++ app/src/main/java/com/base/hamoud/chronictrack/ui/main/MainViewModel.kt
@@ -1,49 +1,31 @@
 package com.base.hamoud.chronictrack.ui.main
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import com.base.hamoud.chronictrack.data.TokesDatabase
+import com.base.hamoud.chronictrack.BaseAndroidViewModel
 import com.base.hamoud.chronictrack.data.entity.Hit
 import com.base.hamoud.chronictrack.data.entity.User
 import com.base.hamoud.chronictrack.data.repository.HitRepo
 import com.base.hamoud.chronictrack.data.repository.UserRepo
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import java.util.*
-import kotlin.coroutines.CoroutineContext
 
-class MainViewModel(application: Application) : AndroidViewModel(application) {
-
-    private var parentJob = Job()
-
-    private val coroutineContext: CoroutineContext
-        get() = parentJob + Dispatchers.IO
-
-    private val scope = CoroutineScope(coroutineContext)
-
-    private val uiScope = CoroutineScope(Dispatchers.Main)
+class MainViewModel(application: Application) : BaseAndroidViewModel(application) {
 
     // set logged in user
     private var user: User = User(UUID.randomUUID().toString(), "Chron")
-    
-    var userRepo: UserRepo
-    var hitRepo: HitRepo
-    var db: TokesDatabase = TokesDatabase.getInstance(application)
+
+    var userRepo: UserRepo = UserRepo(db.userDao())
+    var hitRepo: HitRepo = HitRepo(db.hitDao())
 
     var loggedInUserLive: MutableLiveData<User> = MutableLiveData()
     var userHitsListLive: MutableLiveData<List<Hit>> = MutableLiveData()
 
     init {
-        userRepo = UserRepo(db.userDao())
-        hitRepo = HitRepo(db.hitDao())
-
         attemptInsertUser(user)
-
-        Timber.i("init")
+        Timber.i("attemptInsertUser: $user")
     }
 
     override fun onCleared() {
@@ -52,20 +34,20 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     fun refreshHitsList() {
-        scope.launch {
+        ioScope.launch {
             val hits = hitRepo.getAllHits()
             Timber.i("hit count: ${hits.count()}")
             userHitsListLive.postValue(hits)
         }
     }
 
-    fun insertHit(hit: Hit) = scope.launch(Dispatchers.IO) {
+    fun insertHit(hit: Hit) = ioScope.launch {
         hitRepo.insert(hit)
         refreshHitsList()
     }
 
     private fun attemptInsertUser(user: User) {
-        scope.launch(Dispatchers.IO) {
+        ioScope.launch(Dispatchers.IO) {
             loggedInUserLive.postValue(
                 userRepo.insert(user)
             )
