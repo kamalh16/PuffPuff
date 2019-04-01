6c5a682cb6b388f425bd614efd949b423521d10e
Kamal <kamal.h16@gmail.com>
2019-03-31 20:58:05 -0400

    #29 -  add ability to use app shortcuts to get to add toke screen, toke log, and home

--- app/src/main/java/com/base/hamoud/chronictrack/ui/main/MainNavScreen.kt
+++ app/src/main/java/com/base/hamoud/chronictrack/ui/main/MainNavScreen.kt
@@ -5,6 +5,7 @@ object MainNavScreen {
     const val HOME_SCREEN = "HomeScreen"
     const val TOKE_LOG_SCREEN = "TokeLogScreen"
     const val SETTINGS_SCREEN = "SettingsScreen"
+    const val ADD_TOKE_SCREEN = "AddTokeScreen"
 
     val rootScreenList = listOf(
           HOME_SCREEN, TOKE_LOG_SCREEN, SETTINGS_SCREEN
